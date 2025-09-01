65c3cbd9288bed7f252c0bb019c7973b8e60187d
almamutuguti <alma.wanjiru8j@gmail.com>
2025-09-01 09:19:28 +0300

    introduction to python

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 print("Hello World")
-name = input("What is your name?")
+name = (input("What is your name? "))
 print("Hello" + " " + name)
 
-
+length = (len(name))
+print("Your name has " + str(length) + " letters" )
